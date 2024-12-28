b59721ac28d0016d43b0f2d09be7ad2281dac8a5
Hyeonmin <10582965+hyeonmin-yoo@users.noreply.github.com>
2024-12-28 16:55:25 +0900

    Move code from AppDelegate to SceneDelegate

--- iOSKit/AppDelegate.swift
+++ iOSKit/AppDelegate.swift
@@ -9,15 +9,8 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let rootViewController = ViewController() // ViewController는 직접 생성한 클래스
-        window?.rootViewController = rootViewController
-        window?.makeKeyAndVisible()
-        
         return true
     }
 
